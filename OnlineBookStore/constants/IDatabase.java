004e83f3ad4688c2e6358d2ef0bf199e2d235301
MoyankGiri <56188720+MoyankGiri@users.noreply.github.com>
2022-05-07 21:25:03 +0530

    Added complete project

---
+++ OnlineBookStore/constants/IDatabase.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class IDatabase {
+
+	public static final String DRIVER_NAME = "org.postgresql.Driver";
+	public static final String CONNECTION_STRING = "jdbc:postgresql://localhost:5432/postgres";
+	public static final String USER_NAME = "postgres";
+	public static final String PASSWORD = "postgres";
+
+}
